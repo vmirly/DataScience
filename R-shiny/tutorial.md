177386ec932ca2654d7c16b15f400aa25ecbe72e
mirjalil <vmirjalily@gmail.com>
2014-05-05 22:56:55 -0400

    update to data-science

---
+++ R-shiny/tutorial.md
@@ -0,0 +1,7 @@
+## Tutorial: shiny
+
+Every Shiny app has the same structure: two R scripts saved together in a directory. ui.R and server.R
+
+To build a new app, put the scripts in a new directory, for example called "my_app". and then run it by runApp("my_app")
+
+
---
+++ app-1/ui.R
@@ -0,0 +1,25 @@
+library(shiny)
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+
+  # Application title
+  titlePanel("Hello World from Shiny!"),
+
+  # Sidebar with a slider input for the number of bins
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("bins",
+                  "Number of bins:",
+                  min = 5,
+                  max = 50,
+                  value = 30)
+    ),
+
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotOutput("distPlot")
+    )
+  )
+))
+
