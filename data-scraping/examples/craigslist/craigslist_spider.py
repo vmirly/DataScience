db9f0976e5bf3880b1ce8ef1305e3b83f301f79c
mirjalil <mirjalil@msu.edu>
2015-11-07 22:15:41 -0500

    Data scraping examples

---
+++ data-scraping/examples/craigslist/craigslist_spider.py
@@ -0,0 +1,21 @@
+import scrapy
+
+
+class StackOverflowSpider(scrapy.Spider):
+    name = 'craigslist'
+    start_urls = ['http://lansing.craigslist.org/search/cto']
+
+    def parse(self, response):
+        for href in response.css(''):
+            full_url = response.urljoin(href.extract())
+            yield scrapy.Request(full_url, callback=self.parse_question)
+
+    def parse_question(self, response):
+        yield {
+            'title': response.css('h1 a::text').extract()[0],
+            'votes': response.css('.question .vote-count-post::text').extract()[0],
+            'body': response.css('.question .post-text').extract()[0],
+            'tags': response.css('.question .post-tag::text').extract(),
+            'link': response.url,
+        }
+
