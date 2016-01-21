fedf2facf48fcfabbd29c85d9d29625f46799bf3
mirjalil <mirjalil@msu.edu>
2016-01-21 14:13:55 -0500

    Notebook sorting algorithms

--- data/mics/socket1.py
+++ data/mics/socket1.py
@@ -1,10 +1,10 @@
 import socket
 
 mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-mysock.connect(('http://www.pythonlearn.com', 80))
+mysock.connect(('www.pythonlearn.com', 80))
 mysock.send('GET http://www.pythonlearn.com/code/intro-short.txt HTTP/1.0\n\n')
 
-while True:
+while not True:
     data = mysock.recv(512)
     if ( len(data) < 1 ) :
         break
