1972e469866dc8c28e76a2f5208d79c735ecd1ef
mirjalil <mirjalil@msu.edu>
2016-03-27 13:14:18 -0400

    datastructures: binary_search_trees implement

---
+++ algorithms/c++/main_bst.cpp
@@ -0,0 +1,15 @@
+//
+//
+
+#include <cstdlib>
+#include <iostream>
+#include <ctime>
+#include "mybst.h"
+
+
+using namespace std;
+int main() {
+    BinarySearchTree<int> mytree;
+
+    mytree.insert(5);
+}
--- algorithms/c++/mybst.h
+++ algorithms/c++/mybst.h
@@ -1,17 +1,56 @@
 #ifndef MYBST_H
 #define BST_H
 
-template<typename Type>
+template<typename Comparable>
 class BinarySearchTree {
     private:
         class TreeNode {
             friend class BinarySearchTree;
-            Type  _element;
+            Comparable  _element;
             TreeNode *_left;
             TreeNode *_right;
+
+            TreeNode(Comparable value, TreeNode *left=nullptr, TreeNode *right=nullptr) {
+                _element = value;
+                _left = left;
+                _right = right;
+            }
         };
+
+        TreeNode *root;
+
     public:
-    
+
+       BinarySearchTree();
+       void insert(Comparable);
+       void insert(TreeNode *&, Comparable);
 };
+
+
+template<typename Comparable>
+BinarySearchTree<Comparable>::BinarySearchTree() {
+    // empty constructor
+}
+
+template<typename Comparable>
+void BinarySearchTree<Comparable>::insert(TreeNode * &tree, Comparable value) {
+    if (!tree) {
+        tree = new TreeNode(value, nullptr, nullptr);
+        return;
+    }
+
+    if (tree->_element < value) 
+        insert(tree->_left, value);
+    else if(tree->_element > value) 
+        insert(tree->_right, value);
+    else
+        ; // duplicate value: do nothing
+}
+
+
+template<typename Comparable>
+void BinarySearchTree<Comparable>::insert(Comparable value) {
+    insert(root, value);
+}
 
 #endif
