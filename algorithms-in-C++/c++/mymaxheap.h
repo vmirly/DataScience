d9081354641306e162192f9b2d24e5ebe4fb6ab5
mirjalil <mirjalil@msu.edu>
2016-04-22 10:31:01 -0400

    max-heap impl

--- algorithms-in-C++/c++/mymaxheap.h
+++ algorithms-in-C++/c++/mymaxheap.h
@@ -38,7 +38,10 @@ class myMaxHeap {
 
 
 /* Constructor
- *
+ * @pre: create a max-heap
+ * @param: a vector of comparable type
+ * @post: creates a max-heap
+ * @return: none
  */
 
 template<typename Type>
@@ -54,12 +57,26 @@ myMaxHeap<Type>::myMaxHeap(const std::vector<Type>& items)
 
 
 /*** Private Methods ***/
+
+/* 
+ * @pre: keep a max-heap
+ * @param: none
+ * @post: fix max-heap
+ * @return: none
+ */
 template<typename Type>
 void myMaxHeap<Type>::heapify() {
     for (int i = current_size / 2; i > 0; --i)
         percolateDown(i);
 }
 
+
+/** 
+ * @pre: keep a max-heap
+ * @param: position to start percolation process
+ * @post: keep a max-heap
+ * @return: none
+ */
 template<typename Type>
 void myMaxHeap<Type>::percolateDown(int pos) {
     int child_idx;
@@ -81,6 +98,12 @@ void myMaxHeap<Type>::percolateDown(int pos) {
     data[pos] = std::move(element);
 }
 
+/** 
+ * @pre: keep max-heap
+ * @param: position to start the percolation process
+ * @post: fix the max-heap
+ * @return: none 
+ */
 template<typename Type>
 void myMaxHeap<Type>::percolateUp(int pos) {
     int parent_idx;
@@ -92,8 +115,16 @@ void myMaxHeap<Type>::percolateUp(int pos) {
     data[pos] = std::move(data[0]);
 }
 
+
 /*** Public Methods ***/
 
+
+/** 
+ * @pre: none
+ * @param: none
+ * @post: print the element in the max-heap
+ * @return: none 
+ */
 template<typename Type>
 void myMaxHeap<Type>::display() const {
     for (int i=1; i<=current_size; ++i) {
@@ -102,13 +133,24 @@ void myMaxHeap<Type>::display() const {
 }
 
 
-
+/** 
+ * @pre: none
+ * @param: none
+ * @post: none
+ * @return: true if heap is empty, false otherwise
+ */
 template<typename Type>
 bool myMaxHeap<Type>::isEmpty() const {
     return (current_size == 0);
 }
 
 
+/** 
+ * @pre: none
+ * @param: none
+ * @post: none 
+ * @return: the max-value in the array (root of the tree)
+ */
 template<typename Type>
 Type myMaxHeap<Type>::getMax() const {
     if (current_size == 0) {
@@ -118,12 +160,23 @@ Type myMaxHeap<Type>::getMax() const {
     return data[1];
 }
 
-
+/** 
+ * @pre: none
+ * @param: none
+ * @post: none
+ * @return: the current size of heap 
+ */
 template<typename Type>
 int myMaxHeap<Type>::getSize() const {
     return current_size;
 }
 
+/** 
+ * @pre: none
+ * @param: position of the heap array
+ * @post: none
+ * @return: the element stored at that position
+ */
 template<typename Type>
 Type myMaxHeap<Type>::getItem(int pos) const {
     if (pos > current_size) {
@@ -136,6 +189,12 @@ Type myMaxHeap<Type>::getItem(int pos) const {
 
 /*** Mutators ***/
 
+/** 
+ * @pre: array must remain as max-heap
+ * @param: input x 
+ * @post: adds the element to heap array
+ * @return: none 
+ */
 template<typename Type>
 void myMaxHeap<Type>::insert(const Type& x) {
     if (current_size == data.size() - 1) {
@@ -143,18 +202,17 @@ void myMaxHeap<Type>::insert(const Type& x) {
     }
 
     int pos = ++current_size;
-    //Type xtmp = x;
 
     data[pos] = std::move(x);
     percolateUp(pos);
-    /*data[0] = std::move( xtmp );
-    for ( ; x > data[(int) (pos / 2)]; pos /= 2) {
-        data[pos] = std::move( data[pos / 2] );
-    }
-    
-    data[pos] = std::move( data[0] );*/
 }
 
+/** 
+ * @pre: heap must remain as max-heap
+ * @param: position and a positive value 
+ * @post: increase the key stored at position by positive value
+ * @return: none 
+ */
 template<typename Type>
 void myMaxHeap<Type>::increaseKey(int pos, Type positiveValue) {
     if (positiveValue < 0) {
@@ -164,7 +222,12 @@ void myMaxHeap<Type>::increaseKey(int pos, Type positiveValue) {
     percolateUp(pos);
 }
 
-
+/** 
+ * @pre: heap must remain as max-heap
+ * @param: position and a positive value
+ * @post: decrease he key stored at position by positive value
+ * @return: none 
+ */
 template<typename Type>
 void myMaxHeap<Type>::decreaseKey(int pos, Type positiveValue) {
     if (positiveValue < 0) {
@@ -174,6 +237,12 @@ void myMaxHeap<Type>::decreaseKey(int pos, Type positiveValue) {
     percolateDown(pos);
 }
 
+/** 
+ * @pre: hea must remain as max-heap
+ * @param: none
+ * @post: delete the root of heap tree
+ * @return: none 
+ */
 template<typename Type>
 void myMaxHeap<Type>::deleteMax() {
     if (isEmpty()) {
@@ -185,6 +254,12 @@ void myMaxHeap<Type>::deleteMax() {
     percolateDown(1);
 }
 
+/** 
+ * @pre: heap must remain max-heap
+ * @param: none
+ * @post: delete the root
+ * @return: return the root (max value) 
+ */
 template<typename Type>
 Type myMaxHeap<Type>::extractMax() {
     Type max_element = std::move(getMax());
@@ -192,6 +267,12 @@ Type myMaxHeap<Type>::extractMax() {
     return max_element;
 }
 
+/** 
+ * @pre: heap must remain as max-heap
+ * @param: position
+ * @post: remove element stored at position
+ * @return: none 
+ */
 template<typename Type>
 void myMaxHeap<Type>::remove(int pos) {
     if (pos > current_size) {
