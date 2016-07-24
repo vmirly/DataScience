1d75d04047fa095af38399468ffe828efa7c05be
mirjalil <mirjalil@msu.edu>
2016-07-23 22:04:39 -0400

    computer-vision face-landmarks

---
+++ notebooks/computer-vision/opencv/00-display-image/CMakeFiles/runexample.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/runexample.dir/code.cpp.o"
+  "runexample.pdb"
+  "runexample"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/runexample.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
